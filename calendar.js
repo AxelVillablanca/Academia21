dacf0ca1b9f22e2b30000389973fc9fabd03bad0
Axel Villablanca <123591819+AxelVillablanca@users.noreply.github.com>
2023-08-04 17:53:19 -0400

    Add files via upload

--- calendar.js
+++ calendar.js
@@ -2,7 +2,7 @@
 $("#calendar").evoCalendar({
     language: "es", // Establecer el idioma en español
     theme: "Orange Coral",
-    format: "dd MM , yyyy",
+    format: "dd MM , yyyy", //ERROR EN ALGUNOS MESES, ESCRIBIRLOS EN INGLES PARA MAYOR COMPATIBILIDAD
     eventHeaderFormat: "d MM yyyy",
   
     calendarEvents: [
@@ -12,7 +12,7 @@ $("#calendar").evoCalendar({
         description:
           "Webinar Test de 2 Horas que se realizara a las 18:00 hasta las 20:00", // Description of event (optional)
         badge: "Webinar", // Event badge (optional)
-        date: "1/Julio/2023", // Date of event
+        date: "1/August/2023", //ERROR EN ALGUNOS MESES, ESCRIBIRLOS EN INGLES PARA MAYOR COMPATIBILIDAD
         type: "event", // Type of event (event|holiday|birthday)
         color: "#212121", // Event custom color (optional)
         everyYear: false, // Event is every year (optional)
@@ -23,7 +23,7 @@ $("#calendar").evoCalendar({
         description:
           "Webinar Test de 2 Horas que se realizara a las 18:00 hasta las 20:00", // Description of event (optional)
         badge: "Webinar2", // Event badge (optional)
-        date: "31/Julio/2023", // Date of event
+        date: "31/August/2023", //ERROR EN ALGUNOS MESES, ESCRIBIRLOS EN INGLES PARA MAYOR COMPATIBILIDAD
         type: "event", // Type of event (event|holiday|birthday)
         color: "#008DBA", // Event custom color (optional)
         everyYear: false, // Event is every year (optional)
@@ -34,7 +34,7 @@ $("#calendar").evoCalendar({
         description:
           "Webinar Test de 2 Horas que se realizara a las 18:00 hasta las 20:00", // Description of event (optional)
         badge: "Webinar3", // Event badge (optional)
-        date: "16/Julio/2023", // Date of event
+        date: "16/August/2023", //ERROR EN ALGUNOS MESES, ESCRIBIRLOS EN INGLES PARA MAYOR COMPATIBILIDAD
         type: "event", // Type of event (event|holiday|birthday)
         color: "#FF2138", // Event custom color (optional)
         everyYear: false, // Event is every year (optional)
@@ -45,7 +45,7 @@ $("#calendar").evoCalendar({
         description:
           "Webinar Test de 2 Horas que se realizara a las 18:00 hasta las 20:00", // Description of event (optional)
         badge: "Webinar4", // Event badge (optional)
-        date: "20/Julio/2023", // Date of event
+        date: "20/August/2023", //ERROR EN ALGUNOS MESES, ESCRIBIRLOS EN INGLES PARA MAYOR COMPATIBILIDAD
         type: "event", // Type of event (event|holiday|birthday)
         color: "#207E00", // Event custom color (optional)
         everyYear: false, // Event is every year (optional)
